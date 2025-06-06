655457af30f4757406537bf5c1342cf93051a93b
ehsemloh <ehsemloh@gmail.com>
2025-06-07 11:23:50 +1200

    add show transpiled code

--- src/RNLiveCodeEditor.tsx
+++ src/RNLiveCodeEditor.tsx
@@ -1,21 +1,49 @@
-import React, { useCallback, useContext } from "react";
+import React, { useCallback, useContext, useEffect, useRef, useState } from "react";
 import { RNLiveCodeContext } from "./RNLiveCodeProvider";
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { okaidia } from '@uiw/codemirror-theme-okaidia';
 import { quietlight } from '@uiw/codemirror-theme-quietlight';
+import * as Babel from "@babel/standalone";
 
 export type RNLiveCodeEditorProps = {
   width?: string;
   height?: string;
   theme?: string;
-  onCodeChange?: (code: string) => void
+  onCodeChange?: (code: string) => void;
+  showTranspiled: boolean;
 }
 
-export const RNLiveCodeEditor: React.FC<RNLiveCodeEditorProps> = ({ width, height, theme, onCodeChange }) => {
+export const RNLiveCodeEditor: React.FC<RNLiveCodeEditorProps> = ({ width, height, theme, onCodeChange, showTranspiled }) => {
   const { context, dispatch } = useContext(RNLiveCodeContext);
+  const savedCode = useRef(context.code);
+  const [transpiledCode, setTranspiledCode] = useState<undefined | string>(undefined);
+
+  useEffect(() => {
+    if (showTranspiled) {
+      let transpiled = Babel.transform(
+        savedCode.current,
+        {
+          presets: ['env', 'react', [
+            'typescript',
+            { allExtensions: true, isTSX: true, jsxPragma: 'React', jsxPragmaFrag: 'React.Fragment' }
+          ]]
+        }
+      ).code
+
+      console.log(transpiled);
+      setTranspiledCode(transpiled);
+    } else {
+      setTranspiledCode(undefined);
+    }
+  }, [showTranspiled]);
 
   const onChange = useCallback((code) => {
+    if (showTranspiled) {
+      return;
+    }
+
+    savedCode.current = code;
     onCodeChange?.(code);
     dispatch({ type: 'editing', code })
   }, []);
@@ -30,7 +58,8 @@ export const RNLiveCodeEditor: React.FC<RNLiveCodeEditorProps> = ({ width, heigh
       fontSize: '16px',
     }}>
       <CodeMirror
-        value={context.code}
+        readOnly={showTranspiled}
+        value={transpiledCode ?? context.code}
         width={width ?? '600px'}
         height={height ?? '549px'}
         theme={theme === 'dark' ? okaidia : quietlight}
